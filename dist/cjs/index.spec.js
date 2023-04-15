c8a67292c0ca038514600813c160fcdd878262db
nolleh7707 <nolleh7707@gmail.com>
2023-04-15 16:09:39 +0900

    chore: remove dist folder

--- dist/cjs/index.spec.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const _1 = require(".");
-describe("csv-loader", () => {
-    it("should export csv-loader", () => {
-        expect(_1.CsvParser).toBeDefined();
-    });
-});
--- dist/esm/types/csv-parser.d.ts
+++
@@ -1,4 +0,0 @@
-export declare class CsvParser {
-    static CSVtoArray(text: string): string[] | null;
-    static toObject<T>(raw: string): T[];
-}
